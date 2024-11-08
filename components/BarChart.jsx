7cd069643434cbfe8e57ed0b5c4f15f53e970fa2
madhat5 <davidmalary@gmail.com>
2024-11-08 09:31:01 -0500

    fix: update component params

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -2,9 +2,22 @@
 
 import * as d3 from 'd3';
 
-const BarChart = ({ width, height, data }) => {
-  console.log('width', width)
-  console.log('height', height)
+const specs = {
+  size: {
+    width: 800,
+    height: 600,
+  },
+  margin: {
+    top: 10,
+    right: 10,
+    bottom: 20,
+    left: 40,
+  },
+}
+
+const BarChart = ({ data }) => {
+  console.log('width', specs.size.width)
+  console.log('height', specs.size.height)
   console.log('data', data)
 
   return (
