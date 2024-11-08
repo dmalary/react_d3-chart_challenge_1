2b13336cdf768ace1ca864ff47221de476df34a3
madhat5 <davidmalary@gmail.com>
2024-11-08 09:22:50 -0500

    build: build base component

---
+++ components/BarChart.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+import * as d3 from 'd3';
+
+const BarChart = ({ width, height, data }) => {
+  console.log('width', width)
+  console.log('height', height)
+  console.log('data', data)
+
+  return (
+    <div>sim sim salabim</div>
+  )
+
+}
+export default BarChart;
